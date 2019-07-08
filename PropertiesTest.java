17de44ce8092e3abd47a6ae85df7606fbf87825c
zhehongen <zhehongen@163.com>
2019-07-08 15:42:32 +0800

    Update PropertiesTest.java

--- PropertiesTest.java
+++ PropertiesTest.java
@@ -1,1 +1,18 @@
+ Properties properties = new Properties();
+ InputStream inputStream = Object.class.getResourceAsStream("/myConfig.properties");
+  try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+  String time = properties.get("time").toString();
+
+
+
+
+
+
+
+
+
 
